89a9c5bd9bf9ad72c3e412be658a603a5e03000c
Kushi-09 <159606408+Kushi-09@users.noreply.github.com>
2025-03-21 18:30:04 +0530

    Initial commit

---
+++ todolist/tasks/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import generics, permissions
+from .models import Task
+from .serializers import TaskSerializer
+from django.contrib.auth.models import User
+
+class TaskListCreateView(generics.ListCreateAPIView):
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class TaskRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Task.objects.filter(user=self.request.user)
